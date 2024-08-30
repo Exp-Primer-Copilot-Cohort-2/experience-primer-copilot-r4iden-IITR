a2fab7290e47987a674a245a5c7c866a58b21604
Sathvik Duddela <147434853+r4iden-IITR@users.noreply.github.com>
2024-08-30 13:28:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,18 +2,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// get comments from the database
-const { getComments } = require('./database');
-
-app.get('/comments', (req, res) => {
-  // get comments from the database
-  const comments = getComments();
-  
-  // send comments as a response
-  res.send(comments);
-});
-
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+console.log(`Server is running at http://localhost:${port}`);
 });
